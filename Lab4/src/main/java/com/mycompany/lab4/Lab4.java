d979616989b62b0b62be9e1b8b7f73ad327db4c2
Hr_Cos <152603369+nickhr23i@users.noreply.github.com>
2025-03-18 20:39:55 +0200

    modified lab4_compulsory

--- Lab4/src/main/java/com/mycompany/lab4/Lab4.java
+++ Lab4/src/main/java/com/mycompany/lab4/Lab4.java
@@ -4,6 +4,7 @@
 package com.mycompany.lab4;
 
 import java.util.*;
+import java.util.stream.Collectors;
 
 /**
  *
@@ -15,19 +16,17 @@ public class Lab4 {
         Location[] locations = new Location[5];
         locations[0] = new Location("l1", LocationType.FRIENDLY);
         locations[1] = new Location("l2", LocationType.NEUTRAL);
-        locations[2] = new Location("l4", LocationType.FRIENDLY);
+        locations[2] = new Location("l9", LocationType.ENEMY);
         locations[3] = new Location("l3", LocationType.FRIENDLY);
         locations[4] = new Location("l5", LocationType.ENEMY);
-        Set<Location> friendlies = new TreeSet<>();
-        List<Location> enemies = new LinkedList<>();
-        for (int i = 0; i < locations.length; i++) {
-            if (locations[i].getlType() == LocationType.FRIENDLY) {
-                friendlies.add(locations[i]);
-            }
-            if (locations[i].getlType() == LocationType.ENEMY) {
-                enemies.add(locations[i]);
-            }
-        }
+        Set<Location> friendlies;
+        List<Location> enemies;
+        friendlies = Arrays.stream(locations).filter(loc -> loc.getlType() == LocationType.FRIENDLY)
+                .collect(Collectors.toCollection(TreeSet::new));
+        enemies = Arrays.stream(locations).filter(loc -> loc.getlType() == LocationType.ENEMY)
+                .sorted(Comparator.comparing(Location::getlType)
+                        .thenComparing(Location::getName))
+                .collect(Collectors.toCollection(LinkedList::new));
         System.out.println("Friendlies: ");
         for (Location l : friendlies) {
             System.out.println(l.getName());
